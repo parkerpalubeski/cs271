9db27f51c6fdc4ba1b74fe3099f8f7862707b9e5
Parker Palubeski <ptpski@gmail.com>
2025-11-10 17:32:25 -0800

    Added Mult.asm

--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -8,3 +8,23 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+
+@2
+M=0
+
+(LOOP)
+    @1
+    D=M
+    @END
+    D;JEQ
+    @0
+    D=M
+    @2
+    M=D+M
+    @1
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
