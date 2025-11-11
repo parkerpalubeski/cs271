d84e58b3c7b2160e93b7f77b3aaab5989e8fbe71
Parker Palubeski <ptpski@gmail.com>
2025-11-10 20:13:33 -0800

    Added Fill.asm

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -8,4 +8,44 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+//Main Program Loop
+(LOOP)
+    //Get screen address
+    @SCREEN
+    D=A
+    @0
+    M=D
+    //Get Keyboard Status, puts it into the M Register
+    @KBD
+    D=M
+    @1
+    M=-1
+    //Jump to fill with color black if key is pressed
+    @FILL
+    D;JNE
+    //Else - set color to white and Fill
+    @1
+    M=0
+
+//Fills the Screen with color value within the M Register
+(FILL)
+    //Select color
+    @1
+    D=M
+    //Get pixel
+    @0
+    A=M
+    M=D
+    //Move to next Pixel
+    @0
+    D=M+1
+    @KBD
+    D=A-D
+    @0
+    M=M+1
+    //Loop back
+    @FILL
+    D;JGT
+    //Go back to start if screen is full
+    @LOOP
+    0;JMP
