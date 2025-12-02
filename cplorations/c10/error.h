76fcd88d2e8cf111212ec9458ff35c648123c88f
Parker Palubeski - Laptop <ptpski@gmail.com>
2025-12-02 11:33:01 -0800

    Added c10 for later

---
+++ cplorations/c10/error.h
@@ -0,0 +1,27 @@
+/****************************************
+ * C-ploration 9 for CS 271
+ * 
+ * [NAME] PARKER PALUBESKI
+ * [TERM] FALL 2025
+ * 
+ ****************************************/
+
+#ifndef __ERROR_H__
+#define __ERROR_H__
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+enum exitcode {
+    EXIT_INCORRECT_ARGUMENTS = 1,
+    EXIT_CANNOT_OPEN_FILE = 2,
+    EXIT_TOO_MANY_INSTRUCTIONS = 3,
+    EXIT_INVALID_LABEL = 4,
+    EXIT_SYMBOL_ALREADY_EXISTS = 5,
+    EXIT_INVALID_A_INSTR = 6,
+};
+
+void exit_program(enum exitcode code, ...);
+
+#endif
---
+++ cplorations/c10/parser.h
@@ -0,0 +1,78 @@
+/****************************************
+ * C-ploration 9 for CS 271
+ * 
+ * [NAME] PARKER PALUBESKI
+ * [TERM] FALL 2025
+ * 
+ ****************************************/
+
+#ifndef __PARSER_H__
+#define __PARSER_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "hack.h"
+#include "symtable.h"
+
+#define MAX_HACK_ADDRESS INT16_MAX
+#define MAX_INSTRUCTIONS MAX_HACK_ADDRESS
+#define MAX_LINE_LENGTH  200
+#define MAX_LABEL_LENGTH MAX_LINE_LENGTH - 2
+
+typedef int16_t hack_addr;
+typedef int16_t opcode;
+
+/** function prototypes **/
+char *strip(char *s);
+
+void parse(FILE * file);
+
+bool is_Atype(const char *c);
+bool is_label(const char *c);
+bool is_Ctype(const char *c);
+
+char *extract_label(const char *line, char* label);
+
+typedef int16_t hack_addr;
+typedef int16_t opcode;
+
+enum instr_type{
+    invalid = -1,
+    a_type,
+    c_type,
+};
+
+struct c_instruction{
+    opcode a:1;
+    opcode comp:7;
+    opcode dest:4;
+    opcode jmp: 4;
+};
+
+struct a_instruction{
+    union{
+        char* label;
+        uint16_t hack_addr;
+    };
+    bool is_addr;
+};
+
+struct instruction{
+    union{
+        struct a_instruction a;
+        struct c_instruction c;
+    };
+    enum instr_type type;
+};
+
+void add_predefined_symbols();
+
+bool parse_A_instruction(const char *line, struct a_instruction *instr);
+
+void parse_C_instruction(const char *line, struct c_instruction *instr);
+
+#endif
